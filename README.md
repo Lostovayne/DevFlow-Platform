e84503dd0d2b916cde68014fe0610a21e0ee5ad8
LostoVayne <moises.barrera.dev@gmail.com>
2025-05-16 21:20:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- Instalacion de eslint y reglas de estilo -->
+
+1- pnpm i eslint-config-standard
+2- pnpm i eslint-plugin-n eslint-plugin-promise
+3- pnpm i eslint-plugin-tailwindcss
+4- pnpm i eslint-config-prettier
+5- pnpm i prettier
---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const spaceGrotesk = SpaceGrotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developer from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
+
