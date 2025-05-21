2855a792f337ede2324710db703013f746072bae
LostoVayne <moises.barrera.dev@gmail.com>
2025-05-21 02:09:52 -0400

    refactor(profile): simplify profile page component and use async/await
    Refactor the profile page component to use async/await for better readability and maintainability. Removed redundant elements and streamlined the rendering logic.

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,14 +1,17 @@
 import { notFound } from "next/navigation";
 
-const Profile = ({ params }: { params: { id: string } }) => {
-  if (!params.id) {
-    return notFound();
-  }
+type ProfilePageProps = {
+  params: Promise<{ id: string }>;
+};
 
+const Profile = async ({ params }: ProfilePageProps) => {
+  const { id } = await params;
+  if (!id) {
+    notFound();
+  }
   return (
     <div className="flex flex-col gap-4 px-4 py-6">
-      <h1 className="text-2xl font-bold">User Profile</h1>
-      <p>Profile ID: {params.id}</p>
+      <h1 className="text-2xl font-bold">User Profile {id} </h1>
     </div>
   );
 };
