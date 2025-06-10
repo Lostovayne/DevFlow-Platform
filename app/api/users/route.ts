c106af094e00f0ecb50b6d6f3939d1e9cc5b2ba8
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-10 01:07:02 -0400

    feat(users): implement user API with GET and POST methods and validation schema

---
+++ app/api/users/route.ts
@@ -0,0 +1,32 @@
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const users = await User.find();
+    return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const validateData = UserSchema.safeParse(body);
+
+    if (!validateData.success) {
+      throw new ValidationError(validateData.error.flatten().fieldErrors);
+    }
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
